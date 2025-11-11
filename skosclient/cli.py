b31e1395af9a9e80d4230a0044917f18c9128d11
giacomomarchioro <marchiorogiacomo@gmail.com>
2025-11-11 18:49:34 +0100

    refactoring

--- skosclient/cli.py
+++ skosclient/cli.py
@@ -7,25 +7,8 @@ import argparse
 import sys
 from pathlib import Path
 from tkinter import NO
-from .extractor import SKOSExtractor
-import string
-import importlib.resources as resources
+from .website_generator import generate_website
 
-
-class AtTemplate(string.Template):
-    # Change the delimiter to § because $ and | is used by JavaScript @ by css
-    delimiter = '§'
-    # Use the standard id pattern (match letters/numbers/underscores)
-    idpattern = r'[a-z][_a-z0-9]*'
-
-def render_template(name: str, **kwargs) -> str:
-    try:
-        tpl_text = resources.files("skosclient.websiteresources").joinpath(name).read_text(encoding='utf-8')
-        return AtTemplate(tpl_text).substitute(**kwargs)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Template '{name}' not found in skosclient.websiteresources")
-    except KeyError as e:
-        raise KeyError(f"Missing template variable: {e}")
 
 def main():
     """Main CLI entry point"""
@@ -101,44 +84,7 @@ def main():
     # Create extractor and process
     try:
         print(f"Starting the extractor for creating the SKOS app at {output_path}...")
-        extractor = SKOSExtractor(
-            base_uri=args.base_uri,
-            verbose=args.verbose
-        )
-        str_output_path = str(output_path)
-        print(f"Processing {input_path}...")
-        result = extractor.extract(
-            input_file=str(input_path),
-            output_dir=str_output_path,
-            file_format=args.format
-        )
-        
-        print(f"Successfully processed thesaurus!")
-        print(f"Output saved in: {str_output_path}")
-        print(f"Languages found: {', '.join(result.languages)}")
-        print(f"Total concepts: {result.total_concepts}")
-        print(f"Relations added: {result.total_relations_added}")
-        if result.warnings:
-            print(f"Warnings:")
-            for warning in result.warnings:
-                print(f"   - {warning}")
-
-        #outpath = Path(outdir)
-        #outpath.mkdir(parents=True, exist_ok=True)
-        # breakpoint()
-        # Example usage
-        html = render_template("index.template.html", title="My App", description="/api")
-        (output_path / "index.html").write_text(html)
-
-        for ui_lang in ["en","it","de"]:
-            name = f"ui_translations_{ui_lang}.json"
-            translation = resources.files("skosclient.websiteresources").joinpath(name).read_text(encoding='utf-8')
-            (output_path / name).write_text(translation,encoding="utf-8")
-        # Copy other static files without templating
-        # for static_file in ["script.js", "style.css"]:
-        #    tpl_text = resources.files("skosclient.websiteresources").joinpath(name).read_text(encoding='utf-8')
-        #    (output_path / static_file).write_text(data)
-
+        generate_website(base_uri=args.base_uri,verobse=args.verbose,output_path=output_path,input_path=input_path,file_format=args.format)
     except KeyboardInterrupt:
         print("\n Operation cancelled by user", file=sys.stderr)
         sys.exit(1)
---
+++ skosclient/website_generator.py
@@ -0,0 +1,62 @@
+
+from .extractor import SKOSExtractor
+import string
+import importlib.resources as resources
+
+
+class AtTemplate(string.Template):
+    # Change the delimiter to § because $ and | is used by JavaScript @ by css
+    delimiter = '§'
+    # Use the standard id pattern (match letters/numbers/underscores)
+    idpattern = r'[a-z][_a-z0-9]*'
+
+
+def render_template(name: str, **kwargs) -> str:
+    try:
+        tpl_text = resources.files("skosclient.websiteresources").joinpath(
+            name).read_text(encoding='utf-8')
+        return AtTemplate(tpl_text).substitute(**kwargs)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"Template '{name}' not found in skosclient.websiteresources")
+    except KeyError as e:
+        raise KeyError(f"Missing template variable: {e}")
+
+
+def generate_website(base_uri, verobse, output_path, input_path, file_format):
+    extractor = SKOSExtractor(base_uri=base_uri, verbose=verobse)
+    str_output_path = str(output_path)
+    print(f"Processing {input_path}...")
+    result = extractor.extract(
+        input_file=str(input_path),
+        output_dir=str_output_path,
+        file_format=file_format
+    )
+
+    print(f"Successfully processed thesaurus!")
+    print(f"Output saved in: {str_output_path}")
+    print(f"Languages found: {', '.join(result.languages)}")
+    print(f"Total concepts: {result.total_concepts}")
+    print(f"Relations added: {result.total_relations_added}")
+    if result.warnings:
+        print(f"Warnings:")
+        for warning in result.warnings:
+            print(f"   - {warning}")
+
+    # outpath = Path(outdir)
+    # outpath.mkdir(parents=True, exist_ok=True)
+    # breakpoint()
+    # Example usage
+    html = render_template("index.template.html",
+                           title="My App", description="/api")
+    (output_path / "index.html").write_text(html)
+
+    for ui_lang in ["en", "it", "de"]:
+        name = f"ui_translations_{ui_lang}.json"
+        translation = resources.files("skosclient.websiteresources").joinpath(
+            name).read_text(encoding='utf-8')
+        (output_path / name).write_text(translation, encoding="utf-8")
+    # Copy other static files without templating
+    for static_file in ["script.js", "style.css"]:
+        tpl_text = resources.files("skosclient.websiteresources").joinpath(static_file).read_text(encoding='utf-8')
+        (output_path / static_file).write_text(tpl_text,encoding="utf-8")
