944e26f5c93ed07ea180af18c9fbd2c3a75f1df4
giacomodoardo <giacomodoardo@gmail.com>
2026-04-14 16:41:19 +0200

    Add JavaScript files to package data in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url="https://github.com/yourusername/skosclient",
     packages=find_packages(),
     package_data={
-        'skosclient': ['websiteresources/*.html', 'websiteresources/*.json'],
+        'skosclient': ['websiteresources/*.html', 'websiteresources/*.json','websiteresources/*.js'],
     },
     classifiers=[
         "Development Status :: 4 - Beta",
